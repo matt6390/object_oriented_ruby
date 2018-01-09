020df5f5804e40289d4476996682adab377cd8ff
matt6390 <matthewstone639@gmail.com>
2018-01-08 19:47:28 -0600

    java example of writting code

--- employee.rb
+++ employee.rb
@@ -1,5 +1,21 @@
-employee_1 = ["Bruce", "Wayne", 70000, true]
-employee_2 = ["Diana", "Prince", 80000, true]
+# # employee_1 = ["Bruce", "Wayne", 70000, true]
+# # employee_2 = ["Diana", "Prince", 80000, true]
 
-puts employee_1[0] + " " + employee_1[1] + " " + "makes " + employee_1[2].to_s + " a year."
-puts "#{employee_2[0]} #{employee_2[1]} makes #{70000} a year."
+# # puts employee_1[0] + " " + employee_1[1] + " " + "makes " + employee_1[2].to_s + " a year."
+# # puts "#{employee_2[0]} #{employee_2[1]} makes #{70000} a year."
+
+
+
+
+# employee_1 = {:first_name => "Bruce", :last_name => "Wayne", :salary => 70000, :active => true}
+# employee_2 = {:first_name => "Diana", :last_name => "Prince", :salary => 80000, :active => true}
+
+# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
+# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
+
+
+employee_1 = {first_name: "Bruce", last_name: "Wayne", salary: 70000, active: true}   #this is the javascript way of writting this same code
+employee_2 = {first_name: "Diana", last_name: "Prince", salary: 80000, active: true}   #this is the javascript way of writting this same code
+   #this is the javascript way of writting this same code
+puts "#{employee_1[first_name:]} #{employee_1[last_name:]} makes #{employee_1[salary:]} a year."   #this is the javascript way of writting this same code
+puts "#{employee_2[first_name:]} #{employee_2[last_name:]} makes #{employee_2[salary:]} a year."   #this is the javascript way of writting this same code
