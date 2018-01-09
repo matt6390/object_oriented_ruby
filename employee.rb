4d4d334ded458a0bc3fc6effc1cd00ebeada29cd
matt6390 <matthewstone639@gmail.com>
2018-01-08 19:36:23 -0600

    exmple from class

---
+++ employee.rb
@@ -0,0 +1,5 @@
+employee_1 = ["Bruce", "Wayne", 70000, true]
+employee_2 = ["Diana", "Prince", 80000, true]
+
+puts employee_1[0] + " " + employee_1[1] + " " + "makes " + employee_1[2].to_s + " a year."
+puts "#{employee_2[0]} #{employee_2[1]} makes #{70000} a year."
