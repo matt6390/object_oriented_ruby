6a226b173fe23ab65b4e3273c8cfc697a9413938
matt6390 <matthewstone639@gmail.com>
2018-01-08 20:23:05 -0600

    class Employee example

--- employee.rb
+++ employee.rb
@@ -7,15 +7,42 @@
 
 
 
-# employee_1 = {:first_name => "Bruce", :last_name => "Wayne", :salary => 70000, :active => true}
-# employee_2 = {:first_name => "Diana", :last_name => "Prince", :salary => 80000, :active => true}
+# # employee_1 = {:first_name => "Bruce", :last_name => "Wayne", :salary => 70000, :active => true}
+# # employee_2 = {:first_name => "Diana", :last_name => "Prince", :salary => 80000, :active => true}
 
-# puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
-# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
+# # puts "#{employee_1[:first_name]} #{employee_1[:last_name]} makes #{employee_1[:salary]} a year."
+# # puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} a year."
 
 
-employee_1 = {first_name: "Bruce", last_name: "Wayne", salary: 70000, active: true}   #this is the javascript way of writting this same code
-employee_2 = {first_name: "Diana", last_name: "Prince", salary: 80000, active: true}   #this is the javascript way of writting this same code
-   #this is the javascript way of writting this same code
-puts "#{employee_1[first_name:]} #{employee_1[last_name:]} makes #{employee_1[salary:]} a year."   #this is the javascript way of writting this same code
-puts "#{employee_2[first_name:]} #{employee_2[last_name:]} makes #{employee_2[salary:]} a year."   #this is the javascript way of writting this same code
+# employee_1 = {first_name: "Bruce", last_name: "Wayne", salary: 70000, active: true}   #this is the javascript way of writting this same code
+# employee_2 = {first_name: "Diana", last_name: "Prince", salary: 80000, active: true}   #this is the javascript way of writting this same code
+#    #this is the javascript way of writting this same code
+# puts "#{employee_1[first_name:]} #{employee_1[last_name:]} makes #{employee_1[salary:]} a year."   #this is the javascript way of writting this same code
+# puts "#{employee_2[first_name:]} #{employee_2[last_name:]} makes #{employee_2[salary:]} a year."   #this is the javascript way of writting this same code
+
+
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+
+  end 
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+
+  end 
+end
+
+employee_1 = Employee.new("Bruce", "Wayne", 70000, true)
+employee_1.print_info
+
+
+
+
+
+
+
+
---
+++ store_item.rb
@@ -0,0 +1,6 @@
+#You are the owner of a store that sells items (you decide what specifically). Each item has various properties such as color, price, etc.
+
+
+boat = ["name" => "boat", "color" => "red", :price => 10, shelf: 2]
+car = ["name" => "car", "color" => "green", :price => 15, shelf: 1]
+wingsuit = ["name" => "wingsuit", "color" => "rainbow", :price => 25, shelf: 86]
